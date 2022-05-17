fb2c1ff7abda4c9f4a9b788a2efec77f5e0559b5
zyzhang <1450665003@qq.com>
2022-05-17 15:51:26 +0800

    <fix>(all): update all head files

---
+++ main/main.h
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+
+#include "esp_spi_flash.h"
+
+#include "esp_log.h"
+
+#include "driver/uart.h"
+#include "string.h"
+#include "driver/gpio.h"
+
+
+/******************HAL*********************/
+//UART1
+#include "uart.h" 
+
+
+
+
+
+
+/******************APP*********************/
+//OLED
+//#include "oled.h"
+//#include "oledfont.h"
+//#include "Bmp.h"
