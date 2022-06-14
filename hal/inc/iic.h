358e3be4a844d2fdcde0958c8e779717ae4f66ae
zyZhang <1450665003@qq.com>
2022-06-14 19:53:45 +0800

    fix(hal): update iic hal level

---
+++ hal/inc/iic.h
@@ -0,0 +1,21 @@
+#ifndef IIC_H
+#define IIC_H
+
+#define I2C_MASTER_SCL_IO           GPIO_NUM_41      /*!< GPIO number used for I2C master clock */
+#define I2C_MASTER_SDA_IO           GPIO_NUM_42      /*!< GPIO number used for I2C master data  */
+#define I2C_MASTER_NUM              0                          /*!< I2C master i2c port number, the number of i2c peripheral interfaces available will depend on the chip */
+#define I2C_MASTER_FREQ_HZ          400000                     /*!< I2C master clock frequency */
+#define I2C_MASTER_TX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_RX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
+//#define I2C_MASTER_TIMEOUT_MS       1000
+
+#define WRITE_BIT  I2C_MASTER_WRITE /*!< I2C master write */
+#define READ_BIT   I2C_MASTER_READ  /*!< I2C master read */
+#define ACK_CHECK_EN   0x1     /*!< I2C master will check ack from slave*/
+#define ACK_CHECK_DIS  0x0     /*!< I2C master will not check ack from slave */
+#define ACK_VAL    0x0         /*!< I2C ack value */
+#define NACK_VAL   0x1         /*!< I2C nack value */
+
+extern void i2c_master_init();
+
+#endif
