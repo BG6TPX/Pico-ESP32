e018a8c84915e7e7d26b69005184c8161962b3f5
zyZhang <1450665003@qq.com>
2022-06-15 19:24:25 +0800

    fix(main): update cmakelist and include

--- main/main.h
+++ main/main.h
@@ -1,29 +1,32 @@
 #include <stdio.h>
+#include "stdlib.h"
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
 
 #include "esp_spi_flash.h"
-
 #include "esp_log.h"
+#include <esp_err.h>
 
-#include "driver/uart.h"
 #include "string.h"
+#include "stdint.h"
+
 #include "driver/gpio.h"
+#include "driver/uart.h"
+#include "driver/i2c.h"
 
 
 /******************HAL*********************/
 //UART1
 #include "uart.h" 
 
-
+//IIC
+#include "iic.h"
 
 
 
 
 /******************APP*********************/
 //OLED
-//#include "oled.h"
-//#include "oledfont.h"
-//#include "Bmp.h"
+#include "oled.h"
